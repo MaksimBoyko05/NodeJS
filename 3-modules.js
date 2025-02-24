486ecb3e9b185455ac0bd3fdad465636019e4763
MaksimBoyko05 <vladosmakasin1@gmail.com>
2025-02-24 10:18:52 +0200

    lab1

---
+++ 3-modules.js
@@ -0,0 +1,8 @@
+const names = require ('./firstmodule.js');
+const sayHi = require ('./5-utils.js');
+const data = require ('./6-alternativeFlavor.js');
+ require ('./7-mind-grenade.js');
+
+// sayHi('Susan');
+// sayHi(names.john);
+// sayHi(names.peter);   
