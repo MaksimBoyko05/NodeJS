b7b91cc0159eb7f8762d9bd28bfee6135be110f1
Andrii Aheienko <120367362+Shershen152@users.noreply.github.com>
2025-03-01 17:28:52 +0200

    commit msg

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
 const {readFile, writeFile} = require ('fs');
 
-readFile ('C:/Users/AdminX/Desktop/дз/пропрактикум/NodeJS/content/first.txt', 'utf8', (err, result) => {
+readFile ('./content/first.txt', 'utf8', (err, result) => {
     if (err) {
         console.log(err);
         return
     }
     const first = result;
-    readFile ('C:/Users/AdminX/Desktop/дз/пропрактикум/NodeJS/content/second.txt', 'utf8', (err, result) => {
+    readFile ('./content/second.txt', 'utf8', (err, result) => {
         if (err) {
             console.log(err);
             return;
         }
         const second = result;
         writeFile (
-            'C:/Users/AdminX/Desktop/дз/пропрактикум/NodeJS/content/result-async.txt', 
+            './content/result-async.txt', 
             `Here is the result: ${first}, ${second}`, 
             (err, result) => {
             if (err) {
                 console.log(err);
                 return;
             }
-            console.log(result);
+            console.log("Text has merged into one"); //change
         })
     })
 })
