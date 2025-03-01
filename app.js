9458272ea155b10af0c31c700467189c5a88a55e
Andrii Aheienko <120367362+Shershen152@users.noreply.github.com>
2025-03-01 17:12:48 +0200

    hbhb

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 const {readFile, writeFile} = require ('fs');
 
-readFile ('./content/first.txt', 'utf8', (err, result) => {
+readFile ('C:/Users/AdminX/Desktop/дз/пропрактикум/NodeJS/content/first.txt', 'utf8', (err, result) => {
     if (err) {
         console.log(err);
         return
     }
     const first = result;
-    readFile ('./content/second.txt', 'utf8', (err, result) => {
+    readFile ('C:/Users/AdminX/Desktop/дз/пропрактикум/NodeJS/content/second.txt', 'utf8', (err, result) => {
         if (err) {
             console.log(err);
             return;
         }
         const second = result;
         writeFile (
-            './content/result-async.txt', 
+            'C:/Users/AdminX/Desktop/дз/пропрактикум/NodeJS/content/result-async.txt', 
             `Here is the result: ${first}, ${second}`, 
             (err, result) => {
             if (err) {
