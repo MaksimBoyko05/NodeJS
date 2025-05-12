20ea7f6a0235f152c769e5173780c4d90a79cacb
MaksimBoyko05 <vladosmakasin1@gmail.com>
2025-05-12 11:32:53 +0300

    complete lab5

--- app.js
+++ app.js
@@ -1,29 +1,21 @@
 const express = require('express');
-const path = require('path');
-const {products} = require('./node-express-course/02-express-tutorial/data');
-
 const app = express();
-app.get('/', (req, res) => {
-    res.send('<h1>Home Page</h1><a href="/api/products">Products</a>');
-})
+let {people} = require('./data.js');
 
-app.get('/api/products', (req,res) => {
-    const newProducts = products.map((product) => {
-        const {id, name, image} = product;
-        return {id, name, image};
-    })
-    res.json(newProducts);
+app.use(express.static('./node-express-course/02-express-tutorial/methods-public'));
+app.get('/api/people', (req, res) => {
+    res.status(200).json({success: true, data: people});
 })
+//parse form data
+app.use(express.urlencoded({extended: false}));
 
-app.get('/api/products/:productID', (req,res) => {
-    const {productID} = req.params;
-    const singleProduct = products.find((product) => product.id === Number(productID));
-    if (!singleProduct) {
-        return res.status(404).send('Product does not exist');
+app.post('/login', (req, res) =>{
+    const {name} = req.body;
+    if(name){
+        return res.status(200).send(`Welcome ${name}`);
     }
-    res.json(singleProduct);
+    res.status(401).send('Please provide name');
 })
-
 app.listen(5000, () => {
     console.log('Server is running on port 5000'); 
-});
+})
